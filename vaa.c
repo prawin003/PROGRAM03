beb34c92cd53185cbea2c443e2427f4268f443ab
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-19 10:58:40 +0000

    Create vaa.c

---
+++ vaa.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+ int base,exponent;
+ printf("\n Enter the base number");
+ scanf("%d,&base);
+ printf("\n Enter the exponent");
+ scanf("%d",exponent);
+ while(exp=!0)
+ {
+  result*=base;
+  --exponent;
+  }
+  printf("\n answer is...");
+  r
