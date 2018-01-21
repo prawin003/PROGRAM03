db34c8fe31a3fe56e4f40556212fe992ceface6e
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-21 16:17:05 +0530

    Create ionix.c

---
+++ ionix.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main() 
+{ 
+char name[50];
+printf("\nEnter you name :"); 
+scanf("%s",name); 
+printf("%s,"count+1,name); 
+count+=1; 
+{
+if(count<10) 
+printf("THE NAME") 
+} 
+return 0; 
+}
