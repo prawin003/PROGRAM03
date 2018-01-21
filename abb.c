79ee1f08fa871634dae9acc3948adbe52645068d
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-21 13:59:13 +0530

    Create abb.c

---
+++ abb.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void quicksort(int num[25],int first,int last)
+{
+   int i, j, pivot, temp;
+if(first<last)
+{
+      pivot=first;
+      i=first;
+      j=last;
+while(i<j)
+{
+         while(num[i]<=num[pivot]&&i<last)
+            i++;
+         while(num[j]>num[pivot])
+            j--;
+         if(i<j)
+         {
+            temp=num[i];
+            num[i]=num[j];
+            num[j]=temp;
+         }
+      }
+
+      temp=num[pivot];
+      number[pivot]=num[j];
+      number[j]=temp;
+      quicksort(num,first,j-1);
+      quicksort(num,j+1,last);
+    }
+}
+void main()
+{
+   int i, count, num[25];
+   printf("How many elements are u going to enter?: ");
+   scanf("%d",&count);
+   printf("Enter %d elements: ", count);
+   for(i=0;i<count;i++)
+      {
+      scanf("%d",&num[i]);
+      }
+    quicksort(num,0,count-1);
+    printf("Order of Sorted elements: ");
+   for(i=0;i<count;i++)
+      {
+      printf(" %d",num[i]);
+}
+}
