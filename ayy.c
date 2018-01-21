24fa365e64f00f6db170c97877740e28cf0a901a
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-21 14:29:18 +0530

    Create ayy.c

---
+++ ayy.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,y;
+a=1,2,3,4,5,6,7,8,9,10,11,12;
+b=min;
+printf("enter the time");
+scanf("%d%d",&a,&b);
+if(y==a)
+{
+printf("hour");
+}
+else
+{
+printf("min");
+}
