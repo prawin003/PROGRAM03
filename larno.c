6ae6d56eef1016579e210871c8efce72ecbedd37
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-23 11:14:34 +0000

    Update larno.c

--- larno.c
+++ larno.c
@@ -1,7 +1,14 @@
-#include<stdio.c>
+#include<stdio.h>
 int main()
-{
-double n1,n2,n3;
-printf("enter the value");
-scanf("%d",&d);
+    {
+int n1,n2,n3;
+printf("Enter the values");
+scanf("%d%d%d",&n1,&n2,&n3);
+if(n1>=n2&&n1>=n3)
+printf("n1 is greatest",&n1);
+else if(n2>=n1&&n2>=n3)
+printf("n2 is greatest",&n2);
+else
+printf("n3 is greatest",&n3);
+return 0;
 }
