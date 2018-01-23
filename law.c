f8e2135a905eaeba120a9b7de536df6ff33824f2
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-23 11:11:09 +0000

    Update law.c

--- law.c
+++ law.c
@@ -1,4 +1,27 @@
-#include<studio.h>
-main()
-printf("thn number"):
-scanf("the number %D"):
+
+#include <stdio.h>
+
+int main(void) {
+	int l, h, i, count;
+	scanf("%d %d", &l, &h);
+            while(l<h)
+            {
+            	
+                      count = 0;
+
+                      for(i = 2; i <= l/2; ++i)
+                      {
+                      	
+                         if(l % i == 0)
+                          {
+                            count = 1;
+                            break;
+                          }
+                    }
+
+        if (count == 0 && l!=1)
+            printf("%d ", l);
+            ++l;
+            }
+    return 0;
+}
