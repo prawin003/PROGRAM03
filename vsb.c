4575a4f73ffc916562561664f65d08ff482c5672
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-23 11:03:36 +0000

    Update vsb.c

--- vsb.c
+++ vsb.c
@@ -1,1 +1,13 @@
-ennamo etho
+#include <stdio.h>
+int main()
+{
+    int i = 05;
+    int k = 10;
+    printf(" value of i=%d k=%d before swapping", i, k);
+    i = i ^ k;
+    k = i ^ k;
+    i = i ^ k;
+    printf("value of i=%d k=%d after swapping", i, k);
+    return 0;
+    
+}
