129f2aeb9dbb3b096676ba569304c3ba4b700d8a
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-23 10:45:56 +0000

    Update wee.c

--- wee.c
+++ wee.c
@@ -1,4 +1,16 @@
-#<stdio.h>
-()
-saklj
-eeijkn
+#include<stdio.h> 
+int main()
+{
+   int m,n,rem,i;
+ printf("First Range:\n");
+ scanf("%d",&m);
+ printf("Final Range:\n");
+ scanf("%d",&n);
+ printf("\n The Even numbers between the given interval  %d and%d are",m,n); 
+ for(i=m;i<=n;++i)
+{
+ rem=i%2;
+ if(rem==0)
+ printf("\n %d",i);
+ return 0; 
+}}
