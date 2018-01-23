9d613ccbfdd91deb71a7977a5daff8a25d55df5b
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-23 11:18:25 +0000

    Update aps.c

--- aps.c
+++ aps.c
@@ -1,3 +1,13 @@
-rsdjiogfawhfwhj
-\dklfjsio
-djkfh
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,i,j;
+printf("ENTER THE 1ST HOUR AND MIN");
+scanf("%d%d",&a,&b);
+printf("ENTER THE 2ND HOUR AND MIN");
+scanf("%d%d",&c,&d);
+i=a-c;
+j=b-d;
+printf("THE DIFFERENCE BETWEEN TIME %d\t%d",i,j);
+return 0;
+}
