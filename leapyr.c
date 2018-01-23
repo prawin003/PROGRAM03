b720b0ae39b33e75585975febbc4d36172386c1b
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-23 10:53:10 +0000

    Update leapyr.c

--- leapyr.c
+++ leapyr.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+void main()
 {
-kjsd
+int yr;
+printf("\n enter the yaer:");
+scanf("%d",&yr);
+if(yr%4==0)
+printf("\n the given year is a leap year");
+else
+printf("\n the given year is not a leap year"0;
+getch();
 }
