643e5bebd730b0a27787f444f112205eb862466a
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-23 11:01:33 +0000

    Update adadei.c

--- adadei.c
+++ adadei.c
@@ -1,1 +1,11 @@
-ada deiii
+#include<stdio.h>
+int main()
+{int num;
+printf("Enter the number");
+scanf("%d",&num);
+if(num<=10)
+printf("\n%d Yes");
+else
+printf("\n%d is not in the range",num);
+return 0;
+}
