c9180ed2b45ec4d2c844f9d67531d1c09676698e
prawin003 <31506244+prawin003@users.noreply.github.com>
2018-01-25 11:11:38 +0000

    Create piretes.py

---
+++ piretes.py
@@ -0,0 +1,17 @@
+mport math
+def main():
+	 n=int(input())
+	while(n!=0):
+		l=math.sqrt(n)
+		if l==int(l):
+			print(int(l))
+			break
+		else:
+			n=n-1
+	if n==0:
+		print('no')
+    
+try:
+  main()
+except:
+print('invalid')
